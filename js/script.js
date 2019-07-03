297ebdabcb42f1ac3398473ddbef70c1bc5c1426
Marina Kuklina <nelmare@yandex.ru>
2019-07-03 17:58:22 +0400

    добавляет js для модального окна "напишите нам"

--- js/script.js
+++ js/script.js
@@ -1,28 +1,41 @@
 <script>
-      var link = document.querySelector(".button-write");
-      var popup = document.querySelector(".write-us");
-      var close = popup.querySelector(".modal-close");
-      var form = popup.querySelector("form");
-      var username = popup.querySelector("[name=username]");
-      var useremail = popup.querySelector("[name=useremail]");
+  var link = document.querySelector(".button-write");
+  var popup = document.querySelector(".write-us");
+  var close = popup.querySelector(".modal-close");
+  var form = popup.querySelector("form");
+  var username = popup.querySelector("[name=username]");
+  var useremail = popup.querySelector("[name=useremail]");
+  var text = popup.querySelector("[name=write-textarea]");
 
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("show-write-us");
+    username.focus();
+  });
 
-      link.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        popup.classList.add("show-write-us");
-        username.focus();
-});
-      close.addEventListener("click", function (evt) {
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("show-write-us");
+    popup.classList.remove("modal-error");
+  });
+
+  form.addEventListener("submit", function (evt) {
+    evt.preventDefault();
+    if (!username.value || !useremail.value || !text.value) {
+      evt.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (popup.classList.contains("show-write-us")) {
         evt.preventDefault();
         popup.classList.remove("show-write-us");
-});
-
-       form.addEventListener("submit", function (evt) {
-         evt.preventDefault();
-         if (!username.value || !useremail.value) {
-           evt.preventDefault();
-           console.log("Нужно заполнить все поля формы");
-}
-});
-
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
 </script>
